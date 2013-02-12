1db826e2b69466c259f9d6f75a14562cfd65f848
Margus Lind <margus.lind@gmail.com>
2013-02-12 09:31:04 -0800

    Arturas's helloworld merged in

--- Game/src/client/Client.java
+++ Game/src/client/Client.java
@@ -1,30 +1,58 @@
 package client;
 
 import org.newdawn.slick.*;
+import org.lwjgl.input.Mouse;
+
 public class Client extends BasicGame
-{
+{	Image grass = null;
+	Animation square = null;
+	Image sq1 = null;
+	Image sq2 = null;
+	Image transp = null;
+	int mx = 100;
+    int my = 300; //initial values
+	
+	public String mouse = ""; //can't be null
  
   public Client()
   {
-     super("Client");
+     super("rts alpha .000001");
   }
  
   @Override
   public void init(GameContainer gc) throws SlickException
   {
- 
+	  grass = new Image("resources/img/grass800x600.png");
+	  sq1 = new Image("resources/img/sq120x20.png");
+	  sq2 = new Image("resources/img/sq220x20.png");
+	  square = new Animation(new Image[]{sq1,sq2}, 250);
+	  transp = new Image("resources/img/build140x80.png");
   }
  
   @Override
   public void update(GameContainer gc, int delta) throws SlickException
   {
- 
+	  //these are mainly for testing purposes now
+	  //to check the behaviour (take a notice to Y pos)
+	  int xpos = Mouse.getX();
+	  int ypos = Mouse.getY();
+	  mouse = "x: " + xpos +" y: " + ypos;
   }
  
   @Override
   public void render(GameContainer gc, Graphics g) throws SlickException
   {
-     g.drawString("Client Initializing...", 100, 100);
+     //works intuitively - draws on top
+     grass.draw(0,0);
+     
+     if (Mouse.isButtonDown(0)) {
+	     mx = Mouse.getX();
+		 my = Math.abs(Mouse.getY()-600);
+     }
+     square.draw(mx, my); //drawn before transp, can get under it
+     transp.draw(400, 400);
+     g.drawString(mouse, 300, 100);
+     g.drawString("Hello World", 100, 100);
   }
  
   public static void main(String[] args) throws SlickException
